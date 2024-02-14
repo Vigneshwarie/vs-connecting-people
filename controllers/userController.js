c136f3062e44dae37ee4b332ba934038c0aa8d3b
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-02-14 11:22:56 -0500

    Added code to fetch user by Id

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,5 +10,19 @@ module.exports = {
                console.log(err);
                res.status(500).json(err);
           }
+     },
+     // block to get a single user by id
+     async getUserById(req, res) {
+          try {
+               const userData = await User.findOne({ _id: req.params.id });
+               if (!userData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+               }
+               res.json(userData);
+          } catch (err) {
+               console.log(err);
+               res.status(500).json(err);
+          }
      }
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 // Importing all the method from the userController
-const { getAllUsers } = require('../../controllers/userController');
+const { getAllUsers, getUserById } = require('../../controllers/userController');
 // This route is to get all the users
 router.route('/').get(getAllUsers);
+// This route is to get a single user by id
+router.route('/:id').get(getUserById);
 
 module.exports = router;
